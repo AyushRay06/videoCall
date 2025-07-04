4b66ed7f5bca8de780ea3d593183d8245b1812a3
AyushRay06 <rayayush47@gmail.com>
2025-07-04 16:44:38 +0530

    user-schema

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,40 @@
-export const signup = () => {
+import User from "../modals/User"
+
+export const signup = async (req, res) => {
   try {
-    const { fullname, email, password } = req.body
+    const { fullName, email, password } = req.body
+    if (!fullName || !email || password) {
+      return res.status(401).json({ msh: "All fields are required" })
+    }
+
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast characters" })
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ message: "Invalid email format." })
+    }
+
+    const existingUser = await User.findOne({
+      email,
+    })
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User Already exist." })
+    }
+
+    const idx = Math.floor(Math.random() * 100) + 1 // generate a num between 1-100
+    const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`\
+
+        const newUser = await User.create({
+      email,
+      fullName,
+      password,
+      profilePic: randomAvatar,
+    });
   } catch (error) {}
 }
 
--- backend/src/modals/User.js
+++ backend/src/modals/User.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+import bcrypt from "bcryptjs"
 const UserSchema = new mongoose.Schema(
   {
     fullName: {
@@ -40,6 +40,28 @@ const UserSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   { timestamps: true }
 )
+
+const User = mongoose.modelNames("User", UserSchema)
+
+//pre-hook(ie Do this before savinf the user data)
+
+UserSchema.pre("save", async (next) => {
+  try {
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+    next()
+  } catch (error) {
+    next(error)
+  }
+})
+
+export default User
