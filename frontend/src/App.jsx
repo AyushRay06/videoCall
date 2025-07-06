71b9057fdd5b6e2819c1519d009a7b7dfc992296
AyushRay06 <rayayush47@gmail.com>
2025-07-07 04:22:55 +0530

    daisy-ui-setup-w-themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,17 @@ function App() {
 
   return (
     <>
-      <div className="text-yellow-200">hello</div>
+      <div data-theme="coffee">
+        <div className="text-yellow-200">hello</div>
+        <button class="btn btn-neutral">Neutral</button>
+        <button class="btn btn-primary">Primary</button>
+        <button class="btn btn-secondary">Secondary</button>
+        <button class="btn btn-accent">Accent</button>
+        <button class="btn btn-info">Info</button>
+        <button class="btn btn-success">Success</button>
+        <button class="btn btn-warning">Warning</button>
+        <button class="btn btn-error">Error</button>
+      </div>
     </>
   )
 }
